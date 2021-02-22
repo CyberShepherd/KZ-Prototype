69dbb46d0367913bcc4794255ff4554d48de1897
CyberShephard <g.modzelewski@protonmail.com>
2021-02-22 16:21:35 +0100

    Initial commit 2

---
+++ src/main/java/com/cybershepherd/KZPrototype/KzPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.cybershepherd.KZPrototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KzPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KzPrototypeApplication.class, args);
+	}
+
+}
