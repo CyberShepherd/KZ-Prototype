fbf0519aab50fbf9b9103c44e8e60bbb87f5a856
CyberShephard <g.modzelewski@protonmail.com>
2021-02-24 17:59:16 +0100

    Added missing annotations

--- src/main/java/com/cybershepherd/KZPrototype/lodge/LodgeController.java
+++ src/main/java/com/cybershepherd/KZPrototype/lodge/LodgeController.java
@@ -1,9 +1,12 @@
 package com.cybershepherd.KZPrototype.lodge;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/lodge")
+@AllArgsConstructor
 public class LodgeController {
+    private final LodgeService service;
 }
