b7eef77e43d4f21545ac29800036469df972a5e2
CyberShephard <g.modzelewski@protonmail.com>
2021-02-24 17:36:08 +0100

    Added main controller

---
+++ src/main/java/com/cybershepherd/KZPrototype/MainController.java
@@ -0,0 +1,15 @@
+package com.cybershepherd.KZPrototype;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/")
+public class MainController {
+    @GetMapping
+    public String getHome(){
+        return "home";
+    }
+
+}
