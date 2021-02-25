5303f86454452707daf60987037474b7d24ff881
CyberShephard <g.modzelewski@protonmail.com>
2021-02-25 11:43:32 +0100

    slider functional

--- src/main/resources/static/slider.js
+++ src/main/resources/static/slider.js
@@ -1,3 +1,39 @@
-var displayedSlide = 1;
-var numberOfSlides = 5;
 
+const emptyDot = '<i class="icon-circle-empty"></i>';
+const fullDot = '<i class="icon-circle"></i>';
+
+const pictures = ['img/winter1.png','img/winter2.png', 'img/winter3.png', 'img/winter4.png', 'img/winter5.png'];
+var displayedSlide = pictures.length;
+
+
+function pictureTimer(){
+    changePicture();
+    let nextPic = function(){
+        pictureTimer();
+    }
+    setTimeout(nextPic, 5000);
+}
+
+function changePicture(index){
+    if(index == undefined) ++displayedSlide;
+    else displayedSlide = index;
+
+    if (displayedSlide >= pictures.length) displayedSlide = 0;
+    let dotSetup = generateDots();
+    $('#slide-dots').html(dotSetup);
+    $('#slider').css('background-image', 'url("' + pictures[displayedSlide] + '")');
+}
+
+function generateDots(){
+    dotsHtml = '';
+    for(let i=0; i<pictures.length; i++){
+        if(displayedSlide == i) dotsHtml += fullDot;
+        else dotsHtml += emptyDot;
+    }
+    return dotsHtml;
+}
+
+const onbuild = function(){
+    pictureTimer();
+}
+$(document).ready(onbuild);
